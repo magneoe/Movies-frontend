cacd04092f771f8b58390f6411fc113acd6f267d
magneoe <magneoe@users.noreply.github.com>
2018-12-03 08:23:54 +0100

    Migrated from flex to grid style

--- src/components/tiles/MovieTile.js
+++ src/components/tiles/MovieTile.js
@@ -4,67 +4,38 @@ import StarRatingComponent from 'react-star-rating-component';
 import './style.css';
 
  
-export class MovieTile extends Component {
 
-    render() {
-        let { title, year, posterUrl, duration, averageRating, onClick } = this.props;
+    export const MovieTile = (props) => {
+        let { title, year, posterUrl, duration, averageRating, onClick } = props;
 
-        const genres = this.props.genres || [];
-        const actors = this.props.actors || [];
+        const genres = props.genres || [];
+        const actors = props.actors || [];
 
         const genreView = genres.map(genre => genre.name).join(', ');
         const actorsView = actors.map(actor => actor.name).join(', ');
 
         return (
-            <div className="tileContainer">
-                <div className="tilePropertyContainer">
-                    <table>
-                        <tbody>
-                            <tr onClick={onClick}>
-                                <td colSpan="2">
-                                    <span className="tileHeader tileTitle">{title}</span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td><span className="tileHeader">Year:</span></td>
-                                <td>{year}</td>
-                            </tr>
-                            <tr>
-                                <td><span className="tileHeader">Duration:</span></td>
-                                <td>{duration} mins</td>
-                            </tr>
-                            <tr>
-                                <td><span className="tileHeader">Genres:</span></td>
-                                <td>{genreView}</td>
-                            </tr>
-                            <tr>
-                                <td><span className="tileHeader">Actors:</span></td>
-                                <td>{actorsView}</td>
-                            </tr>
-                            <tr>
-                                <td colSpan="2"><span className="tileHeader">Avg. rating:</span></td>
-                            </tr>
-                            <tr>
-                                <td colSpan="2">
-                                    <div className="ratingContainer">
+            <div className="tileContainerGrid">
+                <div className="headerArea"><span className="tileHeader tileTitle">{title}</span></div>
+                <div className="infoArea">
+                    <div className="infoAreaContainerGrid">
+                        <div className="infoCellEntry"><span className="tileHeader">Year:</span> {year}</div>
+                        <div className="infoCellEntry"><span className="tileHeader">Duration:</span> {duration} mins</div>
+                        <div className="infoCellEntry"><span className="tileHeader">Genres:</span> {genreView}</div>
+                        <div className="infoCellEntry"><span className="tileHeader">Actors:</span> {actorsView}</div>
+                        <div className="ratingContainer">
                                    <StarRatingComponent
                                     name="avgRating"
-                                    value={averageRating} /* number of selected icon (`0` - none, `1` - first) */
-                                    starCount={10} /* number of icons in rating, default `5` */
-                                    emptyStarColor="#e6e6e6" /* color of non-selected icons, default `#333` */
-                                    editing={false} /* is component available for editing, default `true` */
+                                    value={averageRating} 
+                                    starCount={10} 
+                                    emptyStarColor="#e6e6e6" 
+                                    editing={false} 
                                    />
                                    <span>({averageRating}/10)</span>
-                                   </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                        </div>
+                    </div>
                 </div>
-                <div onClick={onClick} className="posterContainer">
-                    <img alt="Movie poster" src={posterUrl} className="posterImg" />
-                </div>
+                <div onClick={onClick} className="posterArea"><img alt="Movie poster" src={posterUrl} className="posterImg" /></div>
             </div>
         );
     }
-}
